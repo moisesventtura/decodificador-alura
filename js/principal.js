d94598017b5ca786dc964e67469b23d6009db9b4
moisesventtura <moisesventura15@gmail.com>
2022-07-04 21:59:07 -0300

    Eventos com javaScript

--- js/principal.js
+++ js/principal.js
@@ -43,6 +43,44 @@ for(var i = 0; i < pacientes.length  ; i++ ){
     }
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();//Previne o comportamento padrão, que é RECARREGAR A PÁGINA
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    // var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    // imcTd.textContent = nome;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    // pacienteTr.appendChild(nomeTd);
+
+    var tabela = document.querySelector("#tabela-pacientes")
+
+    tabela.appendChild(pacienteTr);
+
+});
+
+
 
 
 
