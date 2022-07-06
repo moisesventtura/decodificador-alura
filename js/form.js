e876821ef6d69a792a85430db07063fcf844e9d9
moisesventtura <moisesventura15@gmail.com>
2022-07-05 21:43:27 -0300

    Eventos com javaScript

--- js/form.js
+++ js/form.js
@@ -1,48 +1,3 @@
-
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
-
-var pacientes = document.querySelectorAll(".paciente");
-
-
-for(var i = 0; i < pacientes.length  ; i++ ){
-
-    var paciente = pacientes[i];
-
-
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-
-    var tdImc = paciente.querySelector(".info-imc");
-
-
-    var pesoEhValido = true;
-    var alturaEhValida = true;
-
-    if(peso <= 0 || peso >= 1000){
-        // console.log("Peso inválido");
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (altura <= 0 || altura >= 3.00){
-        // console.log("Altura inválida");
-        tdImc.textContent = "Altura inválida";
-        alturaEhValida = false;
-        paciente.classList.add("paciente-invalido");
-    }
-
-
-    if (alturaEhValida && pesoEhValido ){
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
-}
-
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();//Previne o comportamento padrão, que é RECARREGAR A PÁGINA
@@ -79,9 +34,3 @@ botaoAdicionar.addEventListener("click", function(event){
     tabela.appendChild(pacienteTr);
 
 });
-
-
-
-
-
-
