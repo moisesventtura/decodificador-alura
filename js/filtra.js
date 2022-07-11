aa6b584ddb64ad3710773fd37167b3713ad7d50f
moisesventtura <moisesventura15@gmail.com>
2022-07-11 20:49:40 -0300

    Filtrando a tabela

---
+++ js/filtra.js
@@ -0,0 +1,28 @@
+//
+
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function (){
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length ; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+
+            if(nome != this.value){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else {
+        for(var i = 0; i < pacientes.length ; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+
+
+})
